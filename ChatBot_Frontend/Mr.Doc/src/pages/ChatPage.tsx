5eb3d0c170525157f2e95ce5e40c7a59e824a667
Sudhanshu Anil Gotefode <127535722+SudhanshuGxYZ@users.noreply.github.com>
2025-02-28 15:45:37 +0530

    Update ChatPage.tsx

--- ChatBot_Frontend/Mr.Doc/src/pages/ChatPage.tsx
+++ ChatBot_Frontend/Mr.Doc/src/pages/ChatPage.tsx
@@ -279,7 +279,7 @@ const ChatPage: React.FC<ChatPageProps> = ({ onLogout }) => {
                 </button>
                 <button
                   onClick={onLogout}
-                  className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100"
+                  className="block w-full text-left px-4 py-2 text-indigo-700 hover:bg-gray-100"
                 >
                   <LogOut className="h-5 w-5 inline-block mr-2" />
                   Logout
